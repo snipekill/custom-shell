887f71147f109a0b6205c944d3e8c42d2785eaa7
snipekill <apoorvg@buyhatke.com>
2020-05-10 15:27:22 +0530

    added ouput func in os shell

--- chap3/custom_shell.c
+++ chap3/custom_shell.c
@@ -11,7 +11,7 @@
 
 #define MAXLINE 80
 
-void parse(char *inp,char **args,int *argc)
+void parse(char *inp,char **args,int *argc,int *command_type,char *fileName)
 {
     int j=0 ;
     char newString[MAXLINE/2+1] ;
@@ -23,6 +23,30 @@ void parse(char *inp,char **args,int *argc)
             newString[j]='\0';
             args[*argc] = (char *)calloc(j+1,sizeof(newString[j])) ;
             strcpy(args[*argc],newString) ;
+			if(strcmp("exit",args[0])==0)
+			{
+				*command_type = 4;
+				break ;
+			}
+			if(strcmp("!!",args[0])==0)
+			{
+				*command_type = 5 ;
+				break ;
+			}
+			else if(strcmp(">",args[*argc])==0)
+			{
+				*command_type = 2 ;
+			}
+			else if(strcmp("<",args[*argc])==0)
+			{
+				*command_type = 3 ;
+			}
+			else if(*command_type == 2 || *command_type == 3)
+			{
+				printf("FILENAME RE : %s\n",args[*argc]) ;
+				strcpy(fileName,args[*argc]) ;
+				break ;
+			}
             (*argc)++ ;
             j=0;
             if(inp[i]=='\0' || inp[i]=='\n')
@@ -41,74 +65,73 @@ int main(void)
     char *args[MAXLINE/2+1] ;
     int should_run = 1 ;
     char his_inp[MAXLINE] ;
-    int history =0 ;
+	his_inp[0] = '\0' ;
+	int command_type = 1 ;
     while (should_run)
     {
         printf("osh>");
         fflush(stdout) ;
         int argc = 0 ;
         char inp[MAXLINE] ;
+		command_type = 1 ;
+		char fileName[20] ;
         fgets(inp,MAXLINE,stdin) ;
-        strcpy(his_inp,inp) ;
-        parse(inp,args,&argc) ;
-        if(strcmp("exit",args[0])==0)
-        {
+        parse(inp,args,&argc,&command_type,fileName) ;
+		fileName[0] = '\0' ;
+        if(command_type == 4)
             break ;
-        }
-        if(strcmp("!!",args[0])==0)
+        else if(command_type == 5)
         {
-            if(history == 0)
+            if(his_inp[0]=='\0')
             {
                 printf("No history available\n") ;
                 continue ;
             }
             else
             {
-                parse(his_inp,args,&argc) ;
+				argc = 0 ;
+                parse(his_inp,args,&argc,&command_type,fileName) ;
             }
-            
-            
         }
-        history = 1;
+		else
+			strcpy(his_inp,inp) ;
         int fwait = 1;
-        if(args[argc-1][0]=='&')
+        if(strcmp("&",args[argc-1])==0)
         {
             fwait = 0 ;
             argc --;
+        }
 
-        }
         args[argc] = NULL ;
-        for(int i=0;i<=argc;i++)
-        {
-            printf("%s",args[i]) ;
-        }
-        if(history == 0)
-                history=1;
+        // for(int i=0;i<=argc;i++)
+        // {
+        //     printf("%s",args[i]) ;
+        // }
+        printf("Command Type : %d\n",command_type) ;
+		printf("File Name : %s\n",fileName) ;
         pid_t child = fork() ;
+
         if(child == 0)
         {
-            // printf("haha %s\n",args[1]);
-            // char t[10] = "ls" ;
-            // char *p[0] ;
-            // p = t;
-            if(args[argc-1][0]=='&')
-                {
-                    fwait = 0 ;
-                    argc --;
-
-                }
-                
-            
-            execvp(args[0],args) ;
+			if(command_type == 2 && fileName[0]!='\0')
+			{
+				int fd = open(fileName,O_CREAT | O_RDWR) ;
+				dup2(fd,STDOUT_FILENO) ;
+			}
+            if(execvp(args[0],args)!=-1)
+				exit(1) ;
+			else printf("Execution Failed\n") ;
+			exit(0) ;
         }
         else if(child>0)
         {
-            printf("wait hai kya %d\n",fwait) ;
+            // printf("wait hai kya %d\n",fwait) ;
              if(fwait)
                 wait(NULL) ;
+			
         }
         // printf("%s\n",*args) ;
     }
-    wait(NULL) ;
+   // wait(NULL) ;
     
 }
